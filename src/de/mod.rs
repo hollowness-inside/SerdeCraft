2ad9975c67cf528688b836ed853fc125b8430948
hollowness-inside <malikov.vp@students.dvfu.ru>
2025-08-11 21:33:18 +1000

    Remove pub(self)

--- src/de/mod.rs
+++ src/de/mod.rs
@@ -2,16 +2,16 @@ mod main;
 pub use main::MinecraftDeserializer;
 
 mod r#enum;
-pub(self) use r#enum::MCEnumAccessor;
+use r#enum::MCEnumAccessor;
 
 mod map;
-pub(self) use map::MCMapAccessor;
+use map::MCMapAccessor;
 
 mod r#struct;
-pub(self) use r#struct::MCStructAccessor;
+use r#struct::MCStructAccessor;
 
 mod seq;
-pub(self) use seq::MCSeqAccessor;
+use seq::MCSeqAccessor;
 
 mod variant;
-pub(self) use variant::MCVariantAccessor;
+use variant::MCVariantAccessor;
